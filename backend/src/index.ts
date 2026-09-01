dc37f82d54d6f4ca837b2f0dd852cce2a1fe9518
willfaa <farywildanyfahmi@gmail.com>
2026-09-01 14:32:02 +0700

    *

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -59,8 +59,6 @@ app.use(
   }),
 );
 
-app.options("*", cors() as any);
-
 // --- GLOBAL MIDDLEWARES ---
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
